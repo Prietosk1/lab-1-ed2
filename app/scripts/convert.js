47ccda79460bf2eccf5f3363fa03c919aa9ce786
Oski_Home <david2005prieto@gmail.com>
2025-09-24 04:08:54 -0500

    feat(metodo de conversión): crea el metodo para convertir los datos de csv a json, junto con nuevos datos como links para las banderas y la media de cambio de temperatura por pais

---
+++ app/scripts/convert.js
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import Papa from 'papaparse';
+import iso from 'iso-3166-1';
+
+const csvFilePath = './app/data/dataset_climate_change.csv';
+
+const csv = fs.readFileSync(csvFilePath, 'utf8');
+
+const result = Papa.parse(csv, { header: true });
+
+const years = result.meta.fields.filter((col) => col.startsWith('F'));
+
+const enriched = result.data.map((row) => {
+  const temperatures = years.map((y) => parseFloat(row[y]));
+
+  const codeISO3 = iso.whereAlpha3(row.ISO3);
+
+  const codeISO2 = codeISO3 ? codeISO3.alpha2.toLowerCase() : 'WLD'; // un = unknown
+  console.log(codeISO2); // br or 'unknown'
+  const flag =
+    codeISO2 == 'WLD' ? null : `https://flagcdn.com/w20/${codeISO2}.png`;
+
+  return {
+    id: parseInt(row.ObjectId, 10),
+    name: row.Country,
+    code: row.ISO3,
+    temperatures,
+    flag,
+    avgTempChange:
+      (parseFloat(row.F2022) - parseFloat(row.F1961)) / (years.length - 1),
+  };
+});
+
+// Guardar JSON resultante
+fs.writeFileSync(
+  './app/data/dataset_climate_change.json',
+  JSON.stringify(enriched, null, 2)
+);
+
+console.log('✅ dataset.json generado con éxito');
