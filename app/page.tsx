701cdcd2b4484adb6bf5d3f954487dd9ff7d1204
Oski_Home <david2005prieto@gmail.com>
2025-10-22 06:26:53 -0500

    feat(pagina principal y de airports): se agrego la pagina principal y la de aeropuertos

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
+'use client';
+import Button from '@/app/ui/Button';
+import Link from 'next/link';
+
 export default function Page() {
-  return <p>Dashboard Page</p>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <h1 className="mb-6 text-center text-4xl font-bold md:text-6xl">
+        Treematric
+      </h1>
+
+      <p className="mb-10 max-w-xl text-center">
+        Texto de placeholder. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident laudantium minus, ad, at repellat tempora nesciunt,
+        sequi quae rem quo beatae quidem consequuntur doloremque officiis ipsum
+        magnam repudiandae soluta. Tempora.
+      </p>
+
+      <div className="flex flex-col gap-4 sm:flex-row">
+        <Link href={'/airports'}>
+          <Button onClick={() => console.log('Ir a árbol')}>
+            Vuelos entre aeropuertos
+          </Button>
+        </Link>
+        <Link href={'/tree'}>
+          <Button
+            variant="secondary"
+            onClick={() => console.log('Ir a grafos')}
+          >
+            Clima de paises
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
 }
